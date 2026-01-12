fc01edfcc36921c74d5c5f8bf2dd4edd12976532
Otavio Ragazzo <otavio@oragazzo.com>
2026-01-12 20:46:54 -0300

    feat(control-plane): init nexus django project - Initialized new Django project `nexus` in control-plane - Created `core` app with default DRF serializers - Removed stale READMEs from collector and manager services

---
+++ control-plane/nexus/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for nexus project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import include, path
+from django.contrib import admin
+
+from nexus.core import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    # Admin route
+    path('admin/', admin.site.urls),
+
+    # API routes
+    path("api/v1/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
